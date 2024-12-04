741b18b1e9ad848841c143a938483723b9d7cd9d
1nfsr <10508406+1nfsr@users.noreply.github.com>
2024-12-05 03:37:00 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/**/*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100%',
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
